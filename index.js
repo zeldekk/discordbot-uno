af39b7929207439402bcc9052e728744766ca899
zeldekk <blazej.glowiak@gmail.com>
2025-04-13 14:59:12 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const { Client, IntentsBitField } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.MessageContent,
+    ]
+});
+
+client.login(token);
