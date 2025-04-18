c75a4aa9c7c5b682c2bf9812de2ab8e5ce7495d5
zeldekk <blazej.glowiak@gmail.com>
2025-04-18 11:03:36 +0200

    some fun + basic functions

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const { Client, IntentsBitField } = require('discord.js');
-const { token } = require('./config.json');
+class UnoCard {
+    constructor(colour, type) {
+        this.colour = colour;
+        this.type = type;
+    }
+}
+
+const { Client, IntentsBitField, ActionRowBuilder, ButtonBuilder, ButtonStyle, MessageFlags } = require('discord.js');
+const { token, client_id } = require('./config.json');
 
 const client = new Client({
     intents: [
@@ -8,6 +15,31 @@ const client = new Client({
         IntentsBitField.Flags.GuildMembers,
         IntentsBitField.Flags.MessageContent,
     ]
+});
+
+let players = [];
+
+client.once('ready', () => {
+    console.log('bot online 🥀💀😡😭🤖');
+})
+
+client.on('messageCreate', message => {
+    if (message.author.bot) return;
+    if (message.author != '952296941339934720') return;
+
+    if (message.content === "startgame") {
+        const joinRow = new ActionRowBuilder().addComponents(new ButtonBuilder().setCustomId('2137').setEmoji('👽').setLabel('Join').setStyle(ButtonStyle.Success));
+
+        message.channel.send({content: "hahaha", components: [joinRow]});
+    }
+});
+
+client.on('interactionCreate', (interaction) => {
+    if (!interaction.isButton) return;
+    if (!interaction.customId == '2137') return;
+
+    players.push(interaction.user.id);
+    interaction.reply({content: "You joined the game", flags: MessageFlags.Ephemeral});
 });
 
 client.login(token);
